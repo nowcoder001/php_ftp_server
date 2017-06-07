453e00ff46cddc320b40b41deb768cb5c52d738e
scjtqs <scjtqs@scjtqs-rmbp>
2017-06-07 13:45:01 +0800

    fix

--- README.md
+++ README.md
@@ -6,5 +6,11 @@
 目前只实现简单的逻辑和功能，后续开发后台管理。<br/>
 webserver的代码来自互联网，bug有，能用就行。<br/>
 本程序依赖php的swoole扩展！<br/>
+<h2 align="center">使用说明</h2>
+在命令行，输入 php index.php </br>
+可以生成用户和密码</br>
+<br/>
+在命令行输入 php MyFtpServer.php <br/>
+即可开启ftp服务，可以配合supervisor使用。
 </p>
 </html>
--- index.php
+++ index.php
@@ -9,25 +9,8 @@
 include_once 'CFtpServer.php';
 $usr=new User();
 $user='scjtqs';
-$pass='iamqiushi!.';
+$pass='123456';
 $home=BASE_PATH.'/test';
 $expired='2018-5-5';
 $usr->addUser($user, $pass, $home, $expired);
 $usr->save();
-// $serv = new swoole_server("127.0.0.1", 9501);
-
-// $array=array(
-//     'default'=>array(
-//         //'home'=>'/var/www/html',
-//         'home'=>'/Library/WebServer/Documents/ftp_server/test/',
-//         'folder'=>array(
-//        // array('path'=>'/var/www/html','access'=>'RWANDLCNDI'),
-//        array('path'=>'/Library/WebServer/Documents/ftp_server/test/'),
-//         ),
-//         'ip'=>array(
-//             'allow'=>array('0.0.0.0'),
-//             'deny'=>array()
-//         )
-//     )
-// );
-// echo json_encode($array);
